ce2011ae4c415d7e1af979ad83fb842954786312
Alonso del Arte <alonso.delarte@gmail.com>
2023-11-03 19:32:19 -0400

    New class

---
+++ WheresAbby/Semaphore.swift
@@ -0,0 +1,14 @@
+//
+//  Semaphore.swift
+//  WheresAbby
+//
+//  Created by Alonso del Arte on 11/3/23.
+//
+
+import Foundation
+
+enum Semaphore {
+    
+    case greenLight, yellowLight, redLight
+    
+}
