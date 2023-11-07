21ab6712fc40b6778e5e00c1a6f56b9e74ec9e52
Alonso del Arte <alonso.delarte@gmail.com>
2023-11-07 14:08:19 -0500

    Passing random phone number excludes 911, 999, etc. test

--- WheresAbby/PhoneNumber.swift
+++ WheresAbby/PhoneNumber.swift
@@ -17,12 +17,31 @@ struct PhoneNumber: CustomStringConvertible, Equatable, Hashable {
     
     let line: Int
     
+    static let exclusionsForRandom: [String] = ["000", "110", "616", "666", "911", "999"]
+    
     var description: String {
         "1 (\(areaCode)) \(prefix)-\(String(format: "%04d", line))"
     }
     
+    private static func includesExclusion(_ number: PhoneNumber) -> Bool {
+        let desc = number.description
+        var flag = false
+        for exclude in exclusionsForRandom {
+            if desc.contains(exclude) {
+                flag = true
+            }
+        }
+        return flag
+    }
+    
     static func random(country: Int = 1, area: Int) -> PhoneNumber {
-        PhoneNumber(areaCode: area, prefix: 100, line: Int.random(in: 0...9999))
+        var number = PhoneNumber(areaCode: area, prefix: 100, line: 0)
+        var exclusionFlag = true
+        while exclusionFlag {
+            number = PhoneNumber(areaCode: area, prefix: Int.random(in: 101...998), line: Int.random(in: 10...9989))
+            exclusionFlag = includesExclusion(number)
+        }
+        return number
     }
     
 }
